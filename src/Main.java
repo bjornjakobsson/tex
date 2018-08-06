98c3f6e35e15b69e26976f8d1fd13ce8bcb5e82b
bjorn <c16bjn@cs.umu.se>
2018-08-06 18:44:57 +0200

    Converted project to maven

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import Controller.Controller;
-
-public class Main {
-    public static void main(String[]args){
-       Thread MainGameThread = new Thread(new Controller(800,600));
-       MainGameThread.start();
-    }
-}
